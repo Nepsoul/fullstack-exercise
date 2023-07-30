bb90f351d973a3777b302071a49915a68e8d757c
nepsoul <nirumagar901@gmail.com>
2023-07-30 13:22:11 +0545

    ex: 1.6: unicafe: display the total onClick number

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,5 +1,37 @@
-const App = () => {
-  return <div>New Project</div>;
-};
+import { useState } from 'react'
 
-export default App;
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const goodHandleOnClick=()=>{
+    setGood(good+1)
+    
+    }
+    const neutralHandleOnClick=()=>{
+      setNeutral(neutral+1)
+      
+      }
+    const badHandleOnClick=()=>{
+      setBad(bad+1)
+      
+      }
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={goodHandleOnClick}>good</button>
+      <button onClick={neutralHandleOnClick}>neutral</button>
+      <button onClick={badHandleOnClick}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
