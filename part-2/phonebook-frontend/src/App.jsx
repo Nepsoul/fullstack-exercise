1f8d05f82ff08a7b753eee93e8e3ffef935a7a87
nepsoul <nirumagar901@gmail.com>
2023-08-22 09:27:13 +0545

    EX: 2.11: fetching data from json-server feat. db.json

--- part-2/phonebook-frontend/src/App.jsx
+++ part-2/phonebook-frontend/src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/filter";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterData, setFilterData] = useState("");
+  console.log(persons, "person from promise");
+
+  useEffect(() => {
+    console.log("effext");
+
+    let promise = axios.get("http://localhost:3001/persons");
+    console.log(promise, "promise in hook");
+
+    let returnedPromise = promise.then((response) => {
+      console.log(response, "response from then");
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+    console.log(returnedPromise, "after fetch the promise");
+  }, []);
 
   const addNewName = (event) => {
     event.preventDefault();
@@ -59,7 +70,7 @@ const App = () => {
         addNewName={addNewName}
       />
       <h2>Numbers</h2>
-      <Persons filterName={filterName}/>
+      <Persons filterName={filterName} />
     </div>
   );
 };
