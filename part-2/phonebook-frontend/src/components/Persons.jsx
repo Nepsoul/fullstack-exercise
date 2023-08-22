ee4c35e92f84b14cca3271a1ffc2a4596caaef45
nepsoul <nirumagar901@gmail.com>
2023-08-22 17:11:02 +0545

    EX: 2.14: confirm delete action feat. window.confirm method

--- part-2/phonebook-frontend/src/components/Persons.jsx
+++ part-2/phonebook-frontend/src/components/Persons.jsx
@@ -1,7 +1,6 @@
 import personService from "../services/persons";
 
 const Persons = ({ filterName, setPersons }) => {
-   
   const toDeleteData = (id) => {
     personService.delData(id).then((response) => {
       const filterData = filterName.filter((person) => person.id !== id);
@@ -17,7 +16,9 @@ const Persons = ({ filterName, setPersons }) => {
             {person.name} {person.number}{" "}
             <button
               onClick={() => {
-                toDeleteData(person.id);
+                window.confirm(`Delete ${person.name}`) === false
+                  ? "" //ternary operator, instead of empty string i.e. "", we can use person.name as well, "" this mean do nothing if false
+                  : toDeleteData(person.id);
               }}
             >
               delete
