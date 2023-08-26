81cdc3de0de7f3e80a09035970d3a6129f5d815e
nepsoul <nirumagar901@gmail.com>
2023-08-27 03:53:07 +0545

    EX: 3.4: implement single data delete from list feat. REST client

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -1,50 +1,62 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const app = express();
 
-const persons=[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get("/",(request,response)=>{
-    response.send("<h2>Hello world<h2/>")
-})
+app.get("/", (request, response) => {
+  response.send("<h2>Hello world<h2/>");
+});
 
-app.get("/info",(req,res)=>{
-    res.send(`Phonebook has info for ${persons.length} people <br/>  <br/>${new Date()}`)
-})
-app.get("/api/persons/",(request,response)=>{
-    response.json(persons) //express auto convert into json format
-    // response.end(JSON.stringify(persons)) //explicitly set res content to manually convert into json format
-})
+app.get("/info", (req, res) => {
+  res.send(
+    `Phonebook has info for ${persons.length} people <br/>  <br/>${new Date()}`
+  );
+});
+app.get("/api/persons/", (request, response) => {
+  response.json(persons); //express auto convert into json format
+  // response.end(JSON.stringify(persons)) //explicitly set res content to manually convert into json format
+});
 
-app.get("/api/persons/:id",(req,res)=>{
-    const id=Number(req.params.id)
-    const person=persons.find(data=>data.id===id)
-    if(person){
-    res.json(person)}
-    else{res.status(404).end()}
-})
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((data) => data.id === id);
+  if (person) res.json(person);
+  else {
+    res
+      .status(404)
+      .json({ error: 404, message: `there is no peroson with id ` + id })
+      .end();
+  }
+});
 
-const PORT=3001
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((data) => data.id !== id);
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
