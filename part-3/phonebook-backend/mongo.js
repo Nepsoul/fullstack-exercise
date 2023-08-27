084b4b98fd5290351b14202209e279b171e18e0e
nepsoul <nirumagar901@gmail.com>
2023-08-27 15:49:25 +0545

    info: fetch all data from database

--- part-3/phonebook-backend/mongo.js
+++ part-3/phonebook-backend/mongo.js
@@ -14,27 +14,36 @@ mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
   name: String,
-  number: String,
+  number: String, //to provide number with character
 });
 
 const Person = mongoose.model("Person", personSchema);
-mongoose.connect(url).then((result) => console.log("connected"));
+
+mongoose.connect(url).then((result) => {
+  console.log("connected");
+});
 
 if (process.argv.length > 3) {
   const person = new Person({
     name: process.argv[3],
     number: process.argv[4],
   });
-  person.save().then((result) => {
-    console.log("person data saved!");
-    mongoose.connection.close();
-  });
+  person
+    .save()
+    .then((result) => {
+      console.log(`added ${result.name} number ${result.number} to phonebook`);
+      mongoose.connection.close();
+    })
+    .catch((error) => console.log("promise rejected: ", error.message));
 }
 
-if (process.argv.length === 3)
-  Person.find({}).then((result) => {
-    result.forEach((person) => {
-      console.log(person);
-    });
-    mongoose.connection.close();
-  });
+if (process.argv.length === 3) {
+  Person.find({})
+    .then((result) => {
+      result.forEach((person) => {
+        console.log(person.name, person.number);
+      });
+      mongoose.connection.close();
+    })
+    .catch((error) => console.log("error: ", error.message));
+}
