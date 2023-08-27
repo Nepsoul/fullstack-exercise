4886abc86fb5d551cc2f96e4943338bf2c8d6c75
nepsoul <nirumagar901@gmail.com>
2023-08-27 09:55:44 +0545

    EX: 3.7-3.8: add logger middleware feat. morgan middleware

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+// app.use(morgan("tiny")); //predefined logger middleware(either "tiny" or custom)
 
-app.use(express.json()); //json-parser (note: without parser, req.body is undefined)
-
+app.use(express.json()); //json-parser (note: without parser, req.body of post api is undefined)
+app.use(
+  morgan(function (tokens, req, res) {
+    //custom logger/request-logger middleware (in terminal give hitted api)
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+    ].join(" ");
+  })
+);
 let persons = [
   {
     id: 1,
@@ -43,6 +58,7 @@ app.get("/api/persons", (request, response) => {
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   const person = persons.find((data) => data.id === id);
+  console.log(person);
   if (person) res.json(person);
   else {
     res
@@ -63,6 +79,7 @@ app.post("/api/persons", (req, res) => {
   newData.id = Math.floor(Math.random() * persons.length * 10000000);
 
   let existedData = persons.find((person) => person.name === newData.name);
+  console.log(newData);
   if (existedData) {
     return res.status(400).json({ error: "name must be unique" });
   }
