9a0eb3cdce1e9cc72280094db3da6e67dc6e750e
nepsoul <nirumagar901@gmail.com>
2023-08-28 21:20:29 +0545

    EX: 3.20: number validation feat. POST api

--- part-3/phonebook-backend/models/person.js
+++ part-3/phonebook-backend/models/person.js
@@ -15,7 +15,16 @@ mongoose
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minLength: 5, required: true },
-  number: String, //to provide number with character
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d{7,8}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
+  },
 });
 
 personSchema.set("toJSON", {
