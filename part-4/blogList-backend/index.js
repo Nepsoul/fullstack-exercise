2a888f6c27cc9681ba80d9a640d55aa7295f390f
nepsoul <nirumagar901@gmail.com>
2023-08-31 14:26:55 +0545

    EX: 4-a: express framework for node.js to implement server and set the port

--- part-4/blogList-backend/index.js
+++ part-4/blogList-backend/index.js
@@ -1,1 +1,12 @@
-console.log("new project")
+const express=require("express")
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.send("<h2>This is new Blog-project</h2>")
+})
+
+
+const PORT=3001
+app.listen(PORT, ()=>{
+    console.log(`Server running on PORT ${PORT}`)
+})
