1185dafdd23a45e34ff0737aee2b27b0b4110c89
nepsoul <nirumagar901@gmail.com>
2023-09-03 15:18:47 +0545

    EX: 4.1: blog app user allow to add blog post

--- part-4/blogList-backend/index.js
+++ part-4/blogList-backend/index.js
@@ -1,12 +1,60 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
-app.get("/",(req,res)=>{
-    res.send("<h2>This is new Blog-project</h2>")
-})
+const mongoUrl = `mongodb+srv://<username>:<password>@cluster0.oxhvxoo.mongodb.net/blogList-app?retryWrites=true&w=majority`;
+mongoose.set("strictQuery", false);
+mongoose.connect(mongoUrl);
 
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
 
-const PORT=3001
-app.listen(PORT, ()=>{
-    console.log(`Server running on PORT ${PORT}`)
-})
+const Blog = mongoose.model("Blog", blogSchema);
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("<h2>This is new Blog-project</h2>");
+});
+
+app.get("/api/blogs", (req, res) => {
+  Blog.find({}).then((blogs) => {
+    console.log(blogs, "blogs");
+    res.json(blogs);
+  });
+});
+
+app.get("/api/blogs/:id", (req, res) => {
+  Blog.findById(req.params.id).then((blog) => {
+    if (blog) {
+      res.json(blog);
+    } else {
+      res.status(404).json({
+        error: 404,
+        message: `There is no blog with id ` + req.params.id,
+      });
+    }
+  });
+});
+
+app.post("/api/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+  console.log(blog, "blog");
+  blog.save().then((result) => {
+    res.status(201).json(result);
+  });
+});
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
