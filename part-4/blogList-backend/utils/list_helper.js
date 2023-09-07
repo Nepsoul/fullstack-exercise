49a08fb2544db6217ffa44ca0cf7d0d7de263efd
nepsoul <nirumagar901@gmail.com>
2023-09-07 05:45:26 +0545

    EX: 4.5: test: favorite blog receive most likes

--- part-4/blogList-backend/utils/list_helper.js
+++ part-4/blogList-backend/utils/list_helper.js
@@ -7,7 +7,32 @@ const totalLikes = (blog) => {
   return blog.reduce((acc, cur) => acc + cur.likes, 0);
 };
 
+const favoriteBlog = (blog) => {
+  let returnBlog = blog.reduce(
+    (acc, item) => {
+      if (acc.likes < item.likes) {
+        return { title: item.title, author: item.author, likes: item.likes };
+      } else {
+        return acc;
+      }
+    },
+    { likes: 0 }
+  );
+  return returnBlog;
+
+  //   let topLikes = 0;
+  //   let topBlog = {};
+  //   for (let i = 0; i < blog.length; i++) {
+  //     if (blog[i].likes > topLikes) {
+  //       topLikes = blog[i].likes;
+  //       topBlog = blog[i];
+  //     }
+  //   }
+
+  //   return { title: topBlog.title, author: topBlog.author, likes: topBlog.likes };
+};
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
