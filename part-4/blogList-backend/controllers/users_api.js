f6ecf260b5e96ff8c72f46ce4c04d03848b8e733
nepsoul <nirumagar901@gmail.com>
2024-01-07 22:49:08 +0545

    EX: part 4.16: username and password must be include, username and ps length must be 3 validation handle by controller and dupkey error handle by errorhandler feat. MongoServerError

--- part-4/blogList-backend/controllers/users_api.js
+++ part-4/blogList-backend/controllers/users_api.js
@@ -3,20 +3,36 @@ const usersRouter = require("express").Router();
 
 const User = require("../models/user");
 
-usersRouter.post("/", async (req, res) => {
+usersRouter.post("/", async (req, res, next) => {
   const { username, name, password } = req.body;
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(password, saltRounds); //one-way hash
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  });
+  try {
+    if (!username || !password) {
+      return res
+        .status(400)
+        .send("Both username and password must be provided");
+    }
 
-  const savedUser = await user.save();
+    if (username.length < 3 || password.length < 3) {
+      return res
+        .status(400)
+        .send("Username and password must be greater than 3 character");
+    }
 
-  res.status(201).json(savedUser);
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds); //one-way hash
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    });
+
+    const savedUser = await user.save();
+
+    res.status(201).json(savedUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
 usersRouter.get("/", async (req, res) => {
--- part-4/blogList-backend/utils/middleware.js
+++ part-4/blogList-backend/utils/middleware.js
@@ -27,7 +27,7 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === "ReferenceError") {
     return response.status(400).json({ error: error.message });
   } else if (error.name === "MongoServerError" && error.code === 11000) {
-    //not working errorhandling
+    //worked after using try/catch in controller
     return response.status(400).json({ error: error.message });
   }
   next(error);
