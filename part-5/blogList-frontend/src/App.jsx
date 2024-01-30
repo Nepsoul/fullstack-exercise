1db9a6fa8f95b3ebbc495031fce83d10797ad85e
nepsoul <nirumagar901@gmail.com>
2024-01-30 14:05:55 +0545

    EX: 5.4: add successful and unsuccessful operation to notify the user

--- part-5/blogList-frontend/src/App.jsx
+++ part-5/blogList-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [message, setMessage] = useState({ message: null, type: null });
 
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
@@ -38,8 +39,23 @@ const App = () => {
       setUser(user);
       setUserName("");
       setPassword("");
+      setMessage({
+        message: `Welcome ${user.name} to the Blog app`,
+        type: "success",
+      });
+      setTimeout(() => {
+        setMessage({ message: null, type: null });
+        // setMessage(null);
+      }, 2000);
     } catch (exception) {
-      console.log("wrong credential");
+      setMessage({
+        message: exception.response.data.error,
+        type: "error",
+      });
+      setTimeout(() => {
+        setMessage({ message: null, type: null });
+        // setMessage(null);
+      }, 2000);
     }
   };
 
@@ -69,21 +85,45 @@ const App = () => {
 
   const logOut = () => {
     window.localStorage.removeItem("loggedBlogAppUser");
+    setMessage({
+      message: `${user.name} logout from Blog app`,
+      type: "success",
+    });
+    setTimeout(() => {
+      setMessage({message:null,type:null});
+      // setMessage(null);
+    }, 2000);
     setUser(null);
   };
 
   const handleBlogCreate = async (event) => {
-    event.preventDefault();
-    const newBlog = {
-      title,
-      author,
-      url,
-    };
-    const createdBlog = await blogService.create(newBlog);
-    setBlogs(blogs.concat(createdBlog));
-    setTitle("");
-    setAuthor("");
-    setUrl("");
+    try {
+      event.preventDefault();
+      const newBlog = {
+        title,
+        author,
+        url,
+      };
+      const createdBlog = await blogService.create(newBlog);
+      setBlogs(blogs.concat(createdBlog));
+      setTitle("");
+      setAuthor("");
+      setUrl("");
+      setMessage({
+        message: `a new blog ${createdBlog.title} by ${createdBlog.author} added`,
+        type: "success",
+      });
+      setTimeout(() => {
+        setMessage({message:null,type:null});
+        // setMessage(null);
+      }, 2000);
+    } catch (exception) {
+      setMessage({ message: exception.response.data.error, type: "error" });
+      setTimeout(() => {
+        setMessage({message:null,type:null});
+        // setMessage(null);
+      }, 2000);
+    }
   };
 
   const blogForm = () => (
@@ -121,6 +161,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <div>{message.message}</div>
       {user === null ? (
         <div>
           <h2>Log in to application</h2>
@@ -131,7 +172,9 @@ const App = () => {
           {user.name} logged in <button onClick={logOut}>Logout</button>
           <h2>create new</h2>
           {blogForm()}
-          <button onClick={handleBlogCreate}>create</button>
+          <button type="submit" onClick={handleBlogCreate}>
+            create
+          </button>
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
