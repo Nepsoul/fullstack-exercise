cc62ef86dfb9229976ea9731b5c1bbcf46ba801d
nepsoul <nirumagar901@gmail.com>
2024-04-02 00:12:21 +0545

    EX: 6.2: chage in function

--- part-6/unicafe-redux/src/main.jsx
+++ part-6/unicafe-redux/src/main.jsx
@@ -7,36 +7,12 @@ import reducer from "./reducer";
 const store = createStore(reducer);
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: "GOOD",
-    });
-  };
-
-  const ok = () => {
-    store.dispatch({
-      type: "OK",
-    });
-  };
-
-  const bad = () => {
-    store.dispatch({
-      type: "BAD",
-    });
-  };
-  
-  const resetStat = () => {
-    store.dispatch({
-      type: "ZERO",
-    });
-  };
-
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button onClick={ok}>ok</button>
-      <button onClick={bad}>bad</button>
-      <button onClick={resetStat}>reset stats</button>
+      <button onClick={()=>store.dispatch({type:"GOOD"})}>good</button>
+      <button onClick={()=>store.dispatch({type:"OK"})}>ok</button>
+      <button onClick={()=>store.dispatch({type:"BAD"})}>bad</button>
+      <button onClick={()=>store.dispatch({type:"ZERO"})}>reset stats</button>
       <div>good {store.getState().good}</div>
       <div>ok{store.getState().ok}</div>
       <div>bad{store.getState().bad}</div>
